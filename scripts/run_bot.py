13b22acc50768ea6cc39181e8a29613ed77773a6
VladisIove <vlad.shelemakha0302@gmail.com>
2019-07-01 07:15:27 +0300

    Add state

--- scripts/run_bot.py
+++ scripts/run_bot.py
@@ -9,66 +9,77 @@ TOKEN = '836473782:AAG61NxDhMys1gKl_uE5rZnVcUolDdZXY-s'
 
 bot = CustomBot(TOKEN)
 
-
+@bot.register_handler_by_state(state='start')	
 @bot.message_handler(commands=['start'])
-def process_state_first_command(message):
+def process_start_command(message):
 	user_id = message.from_user.id
 	user = TGUser.objects.filter(tg_id=user_id).first()
 
 	if not user:
 		user = TGUser(tg_id=user_id)
 
-	user.state = 'start'
+	user.state = 'interesting'
 	user.save()
-	markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+
+	markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
 	markup.add('Продолжить','Не интересно')
-	msg = bot.reply_to(message, 'Мы крутые типы, пилим ботов. Го дальше. не?', reply_markup = markup )
-	bot.register_next_step_handler(msg, process_interesting_step)
+	bot.reply_to(message, 'Мы крутые типы, пилим ботов. Го дальше. не?', reply_markup = markup )
 
 
-	
+
+@bot.register_handler_by_state(state='interesting')	
 def process_interesting_step(message):
+	user_id = message.from_user.id
+	user = TGUser.objects.filter(tg_id=user_id).first()
+
 	chat_id = message.chat.id 
 	interesting = message.text 
 	if interesting == u'Продолжить':
-		msg = bot.reply_to(message, 'Опишите свою сферу деятельности')
-		bot.register_next_step_handler(msg, process_activity_step)
+		bot.reply_to(message, 'Опишите свою сферу деятельности')
+		user.state = 'activity'
+		user.save()
+
 	elif interesting ==  u'Не интересно':
 		msg = bot.reply_to(message, 'До свидания!')
-		markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+		markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
 		markup.add('Cоздать новую заявку')
-		msg = bot.reply_to(message,'Ну так что?', reply_markup = markup)
-		bot.register_next_step_handler(msg, process_state_first_command)
+		bot.reply_to(message,'Ну так что?', reply_markup = markup)
+		user.state = 'start'
+		user.save()
+		
 
+
+
+@bot.register_handler_by_state(state='activity')	
 def process_activity_step(message):
 	user_id = message.from_user.id 
 	user = TGUser.objects.get(tg_id=user_id)
+	user.state = 'order'
+	user.save()
 	order = Order.objects.create(user = user )
 	order.industry = message.text 
 	order.save()
-	msg = bot.reply_to(message, 'Опишите вашу задачу')
-	bot.register_next_step_handler(msg, process_order_step)
+	bot.reply_to(message, 'Опишите вашу задачу')
 
+
+@bot.register_handler_by_state(state='order')	
 def process_order_step(message):
 	user_id = message.from_user.id
 	user = TGUser.objects.get(tg_id=user_id)
 	order = Order.objects.get(user=user, task_description='Empty')
 	order.task_description = message.text
 	order.save()
-	msg = bot.reply_to(message, 'Спасибо за заявку! В ближайшее время с вами свяжутся для обсуждения вашей задачи')
+	bot.reply_to(message, 'Спасибо за заявку! В ближайшее время с вами свяжутся для обсуждения вашей задачи')
 	
-	markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+	markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
 	markup.add('Cоздать новую заявку')
-	msg = bot.reply_to(message,'Еще заказ?', reply_markup = markup)
-	bot.register_next_step_handler(msg, process_state_first_command)
+	bot.reply_to(message,'Еще заказ?', reply_markup = markup)
+	user.state = 'start'
+	user.save()
 
 
 
 
-
-@bot.register_handler_by_state(state='start')
-def process_state_second_command(message):
-	bot.send_message(message.chat.id, 'name-filling state handler called!!!')
 
 
 
